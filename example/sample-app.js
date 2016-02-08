caa211940844abb6c4c54c477b1f4e8b5d8d177e
Ahmed Badawi <ahmedabadawi@gmail.com>
2016-02-08 23:12:05 +0200

    Added auto-refresh with enter key and added some UI enhancements

--- example/sample-app.js
+++ example/sample-app.js
@@ -2,17 +2,34 @@ $(function() {
     var $diagramType = $("#diagramType");
     var $diagramText = $("#diagramText");
     var $diagramArea = $("#diagramArea");
+    
+
+
+    var refreshDiagram = function() {
+        var parser = new uml2svg.parser[$diagramType.val()]();
+        try {
+            var diagramModel = parser.parse($diagramText.val(), "diagram");        
+            // TODO: Handle parsing errors with sticky message showing errors
+            if(diagramModel) {
+                var renderer = 
+                    new uml2svg.Uml2svg($diagramType.val(), { 
+                                                        width:640,
+                                                        height:480 });
+
+                var svg = renderer.render(diagramModel);
+                $diagramArea.html(svg);
+            }
+        } catch(ignore) { }
+    };
 
     $("#render").on("click", function() {
-        var parser = new uml2svg.parser[$diagramType.val()]();
-        var diagramModel = parser.parse($diagramText.val(), "diagram");        
-        var renderer = 
-            new uml2svg.Uml2svg($diagramType.val(), { 
-                                                width:640,
-                                                height:480 });
+        refreshDiagram();
+    });
 
-        var svg = renderer.render(diagramModel);
-        $diagramArea.html(svg);
+    $diagramText.on("keyup", function(e) {
+        if((e.keyCode || e.which) == 13) {
+            refreshDiagram();    
+        }
     });
 });
 
